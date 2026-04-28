768f6c2f3fd5a506d68d3b6fdb91d0c0af8ecd60
singlaharshul123 <125254376+singlaharshul123@users.noreply.github.com>
2026-04-29 02:38:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+import requests
+import time
+
+BOT_TOKEN = "8293946395:AAHLrBFmcAtWiZDideIMqbDoZnl8W7K8si4"
+CHAT_ID = "5007925991"
+
+PRODUCT_CODE = "322042"
+PINCODE = "110001"
+
+def send_telegram(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    try:
+        requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+    except Exception as e:
+        print("Telegram error:", e)
+
+def check_stock():
+    url = f"https://www.croma.com/api/v2/product/{PRODUCT_CODE}?pincode={PINCODE}"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    try:
+        r = requests.get(url, headers=headers, timeout=10)
+        
+        if r.status_code == 200:
+            data = r.text.lower()
+
+            # safer logic
+            if "out of stock" in data:
+                return False
+            elif "available" in data or "in stock" in data:
+                return True
+            else:
+                return False
+
+    except Exception as e:
+        print("Request error:", e)
+
+    return False
+
+
+print("🚀 Started checking...")
+
+while True:
+    if check_stock():
+        print("✅ FOUND IN STOCK!")
+        send_telegram(f"🔥 IN STOCK!\nProduct Code: {PRODUCT_CODE}")
+        break
+    else:
+        print("⏳ checking...")
+
+    time.sleep(5)
